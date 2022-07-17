8197e9e0acc01ae6082af7b1159e5226bd100462
kim-hyunjin <hyunjin1612@gmail.com>
2022-07-17 14:00:07 +0900

    feat: add dummy data file, config image domain

--- discover-coffee-stores/pages/index.tsx
+++ discover-coffee-stores/pages/index.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import Card from '@/components/card';
 import Banner from '@/components/banner';
 
+import coffeeStores from '@/data/coffee-stores.json';
+
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -23,12 +25,15 @@ export default function Home() {
           <Image src={'/static/hero-image.png'} width={700} height={400} alt={'hero'} />
         </div>
         <div className={styles.cardLayout}>
-          <Card
-            className={styles.card}
-            name={'starbucks'}
-            imgUrl={'/static/hero-image.png'}
-            href={'/coffee-store/starbucks'}
-          />
+          {coffeeStores.map((coffeeStore) => (
+            <Card
+              key={coffeeStore.id}
+              className={styles.card}
+              name={coffeeStore.name}
+              imgUrl={coffeeStore.imgUrl}
+              href={`/coffee-stores/${coffeeStore.id}`}
+            />
+          ))}
         </div>
       </main>
     </div>
