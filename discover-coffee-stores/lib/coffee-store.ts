1f477763e7713bef1f307dbb1ee2d003800c4b27
kim-hyunjin <hyunjin1612@gmail.com>
2022-07-23 17:57:29 +0900

    feat: update fetch coffee stores lib - update how to use api result data

--- discover-coffee-stores/lib/coffee-store.ts
+++ discover-coffee-stores/lib/coffee-store.ts
@@ -26,9 +26,13 @@ export const fetchCoffeeStores = async (): Promise<CoffeeStore[]> => {
       name: d.name,
       imgUrl:
         'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80',
-      websiteUrl: 'https://www.strangelovecoffee.ca/',
+      websiteUrl: '',
       address: d.location.formatted_address,
-      neighbourhood: 'at King and Spadina',
+      neighbourhood:
+        d.location.neighborhood?.[0] ||
+        d.related_places?.present?.name ||
+        d.location.post_town ||
+        '',
     }));
   }
   return coffeeStores;
