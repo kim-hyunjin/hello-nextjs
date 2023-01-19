959a28ae0891b0ee8ed6bd00da3c96687bf4ebfc
kim-hyunjin <hyunjin1612@gmail.com>
2023-01-19 15:20:36 +0900

    invoke youtube api

--- youflix/lib/videos.ts
+++ youflix/lib/videos.ts
@@ -1,11 +1,17 @@
-import videoData from '../data/temp.json';
 import Video from '../types/video';
 
-export const getVideos = (): Promise<Video[]> => {
-  return Promise.resolve().then(() => {
-    return videoData.items.map((v) => ({
-      id: v.id.videoId,
-      imgUrl: v.snippet.thumbnails.high.url,
-    }));
-  });
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+const calmdownman_id = 'UCUj6rrhMTR9pipbAWBAMvUQ';
+
+export const getVideos = async (): Promise<Video[]> => {
+  const response = await fetch(
+    `${YOUTUBE_SEARCH_URL}?part=snippet&channelId=${calmdownman_id}&order=date&type=video&maxResults=25&key=${process.env.YOUTUBE_API_KEY}`
+  );
+
+  const data = await response.json();
+
+  return data.items.map((v: any) => ({
+    id: v.id.videoId,
+    imgUrl: v.snippet.thumbnails.high.url,
+  }));
 };
