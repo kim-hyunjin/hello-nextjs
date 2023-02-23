d2710a911beaa99cb2274d347c2171e51aa75dce
kim-hyunjin <hyunjin1612@gmail.com>
2023-02-23 17:23:24 +0900

    use react-modal for video player

--- chimflix/pages/video/[videoId].tsx
+++ chimflix/pages/video/[videoId].tsx
@@ -1,10 +1,30 @@
 import { useRouter } from 'next/router';
+import { useCallback } from 'react';
+import Modal from 'react-modal';
+
+import styles from '@/styles/Video.module.css';
+
+Modal.setAppElement('#__next');
 
 const Video = () => {
   const router = useRouter();
   const { videoId } = router.query;
 
-  return <h1>{videoId}</h1>;
+  const handleClose = useCallback(() => {
+    router.back();
+  }, [router]);
+
+  return (
+    <div>
+      video page {videoId}
+      <Modal
+        isOpen={true}
+        contentLabel='Watch the video'
+        onRequestClose={handleClose}
+        overlayClassName={styles.overlay}
+      />
+    </div>
+  );
 };
 
 export default Video;
